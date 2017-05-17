39281a90336ddb1679877c256931e469456eab85
Dan Taylor <dan@powerchurch.com>
2017-05-17 15:25:47 -0400

    Update person.rb

--- app/models/person.rb
+++ app/models/person.rb
@@ -66,7 +66,7 @@ class Person < ActiveRecord::Base
   validates_format_of :business_website, :allow_nil => true, :allow_blank => true, :with => /^https?\:\/\/.+/
   validates_format_of :business_email, :allow_nil => true, :allow_blank => true, :with => VALID_EMAIL_ADDRESS
   validates_format_of :alternate_email, :allow_nil => true, :allow_blank => true, :with => VALID_EMAIL_ADDRESS
-  validates_inclusion_of :gender, :in => %w(Male Female), :allow_nil => true
+  # validates_inclusion_of :gender, :in => %w(Male Female), :allow_nil => true
   validates_attachment_size :photo, :less_than => PAPERCLIP_PHOTO_MAX_SIZE
   validates_attachment_content_type :photo, :content_type => PAPERCLIP_PHOTO_CONTENT_TYPES
 
